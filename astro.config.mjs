0b68389f560355d8a515d8b6cf87e8a3c708e157
Hike-projects <160869264+Hike-projects@users.noreply.github.com>
2025-02-24 18:44:45 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,11 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://tech-station.vercel.app/',
-	integrations: [mdx(), sitemap()],
+  site: 'https://tech-station.vercel.app/',
+  integrations: [
+    mdx(),
+    sitemap({
+      site: 'https://tech-station.vercel.app/',  // Explicitly add site here
+    }),
+  ],
 });
